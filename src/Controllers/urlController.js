862e4a026226f091492f0bd4fe137f9d814305a5
SaloniDeveloper <91768766+SaloniDeveloper@users.noreply.github.com>
2022-05-19 08:15:07 +0530

    caching

--- src/Controllers/urlController.js
+++ src/Controllers/urlController.js
@@ -2,38 +2,70 @@ const urlModel = require("../model/urlModel")
 const shortid = require("shortid")
 const validUrl = require("valid-url")
 const baseUrl ="http://localhost:3000"
+const redis = require("redis")
+const{promisify} = require("util")
+
+
+//Connect to redis
+const redisClient = redis.createClient(
+    12381,
+    "redis-12381.c301.ap-south-1-1.ec2.cloud.redislabs.com",
+    { no_ready_check: true }
+  );
+  redisClient.auth("TJvkt2L7N6K6bHx4TyCKK8tT0QT1Rtut", function (err) {
+    if (err) throw err;
+  });
+      
+redisClient.on("connect", async function () {
+        console.log("Connected to Redis..");
+    });
+      
+
+//Connection setup for redis     
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+
+
+/****************************** Validation function*********************************/
+let keyValid = function (value) {
+    if (typeof (value) === "undefined" || typeof (value) == null) { return false }
+    if (typeof (value) === "string" && value.trim().length == 0) { return false }
+    return true
+}
+/***********************************************************************************/
 
 
 const createShortUrl = async function(req, res){
     try{
+        let data = req.body
+        const { longUrl } = data
 
-        const { longUrl } = req.body // destructure the longUrl from req.body.longUrl
-        if (!Object.keys(req.body).length) {
-            return res.status(400).send({ status: false, message: "please provide url details" }) 
-        }
-        if (!validUrl.isUri(baseUrl)) { return res.status(400).send('Invalid base URL')} 
-        // check base url if valid using the validUrl.isUri method
-    
-        const urlCode = shortid.generate()    // if valid, we create the url code
-    
-        // check long url if valid using the validUrl.isUri method
-        if (validUrl.isUri(longUrl)) {
-                /* The findOne() provides a match to only the  of thesubset documents 
-                in the collection that match the query. In this case, before creating the short URL,
-                we check if the long URL was in the DB ,else we create it.*/
-                
-                let url = await urlModel.findOne({ longUrl }) 
-    
-                if (url) { res.send(url) }  // url exist and return the respose
-                 else {
-                    const shortUrl = baseUrl + '/' + urlCode   // join the generated short code the the base url
+        if (!Object.keys(data).length || !keyValid(data)) 
+            return res.status(400).send({ status: false, message: "Please enter your URL." }) 
 
-                    url = new urlModel({ longUrl, shortUrl, urlCode, date: new Date() }) // invoking the Url model and saving to the DB
-                    
-                    await url.save()
-                    res.send(url)
-                }
-        } else { res.status(400).send('Invalid longUrl') } 
+        if (!validUrl.isUri(longUrl)) 
+            return res.status(400).send({status: false, message:'Enter a valid URL'})
+
+        if (!validUrl.isUri(baseUrl)) 
+            return res.status(400).send({status: false, message:'Enter a valid base URL'}) 
+
+        let findUrl = await urlModel.findOne({ longUrl: longUrl })
+        if(findUrl)
+            return res.status(200).send({status: true, message: "Already created", data: findUrl})
+    
+        const urlCode = shortid.generate()    
+        const shortUrl = baseUrl + '/' + urlCode
+
+        data.urlCode = urlCode
+        data.shortUrl = shortUrl
+
+        const createUrl = await urlModel.create({urlCode: urlCode, longUrl: longUrl, shortUrl: shortUrl})
+
+        // setting in cache
+        await SET_ASYNC(`${shortUrl}`, JSON.stringify(createUrl))
+        await SET_ASYNC(`${longUrl}`, JSON.stringify(createUrl))
+
+        return res.status(201).send({status: true, message: "Successfully Shorten the URL.", data: createUrl})
 
     }
     catch(error){
@@ -41,18 +73,24 @@ const createShortUrl = async function(req, res){
     }
 }
 
+
 const getUrl=async function(req,res){
     try{
-    let urlCode=req.params.urlCode
-    let url=await urlModel.findOne({urlCode:urlCode})
-    console.log(url)
-    if(url){
-        return res.status(302).redirect(url.longUrl)
-    }
-    return res.status(404).send({status:false,message:"url not found"})
-    }
-    catch(error){
-        res.status(500).send({ status: false, message:error.message})
+        let urlCode= await GET_ASYNC(`${req.params.urlCode}`)
+        if(urlCode){
+            let url = JSON.parse(urlCode)
+            res.status(302).redirect(url.longUrl)
+        }
+        else{
+            let findUrl = await urlModel.findOne({urlCode: req.params.urlCode})
+            if(!findUrl)
+            return res.status(404).send({status:false,message:"url not found"})
+            await SET_ASYNC(`${req.params.urlCode}`, JSON.stringify(findUrl))
+            res.status(302).redirect(findUrl.longUrl)
+        }
+
+    } catch(error) {
+            res.status(500).send({ status: false, message:error.message})
     }
 }
 
